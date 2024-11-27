9eb1e123f0103a01ddee43dba71a3e84b28c0d0c
Igmat68 <strategmim@gmail.com>
2024-11-27 21:48:25 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Как здоровье? ')
+print(14)
+my_string = 'Как хорошо получать новые знания!'
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
