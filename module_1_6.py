a1b50eaaa57f1ef7ed2d29626c59fb9b27d8c68a
Igmat68 <strategmim@gmail.com>
2024-11-27 20:53:57 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+Number_of_comleted_HA = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_fo_one_task = (Number_of_hours_spent / Number_of_comleted_HA)
+print("Курс: ", Course_name, ", всего задач:", Number_of_comleted_HA, ", затрачено часов: ", Number_of_hours_spent, ", среднее время выполнения ", Time_fo_one_task, " часа.", sep="")
