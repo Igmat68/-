fa31801ac21e51dfe6cf1f74df076de9761f12e3
Igmat68 <strategmim@gmail.com>
2024-12-08 17:18:27 +0500

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -3,10 +3,9 @@ a = [42, 69, 322, 13, 99, 0, -5, 9, 8, 7, -6, 5]
 
 i = 0
 while i < len(a):
+    if a[i] > 0:
+        print(a[i])
     if a[i] < 0:
         break
-    if a[i] != 0:
-        print(i, a[i])
-
-        i += 1
+    i += 1
 
